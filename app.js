b7d86a0eb107beb3c13a498358c2d4f9af01aa31
Mohammed Alqadi <120240112@std.up.edu.ps>
2026-09-13 12:01:07 +0300

    fix eslint errprs-1

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
-app.use(express.static("dist"));
+app.use(express.static('dist'))
 
 const start = async () => {
-  await app.listen(PORT);
-  // eslint-disable-next-line no-console
-  console.log(`server started on port ${PORT}`);
-};
+  await app.listen(PORT)
 
-start();
+  console.log(`server started on port ${PORT}`)
+}
+
+start()
