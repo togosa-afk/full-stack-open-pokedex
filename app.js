84f1c79b9cfad4d91275f2c7537bf5ce927d5ba7
Mohammed Alqadi <120240112@std.up.edu.ps>
2026-09-13 17:31:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.get('/version', (req, res) => {
   res.send('2')
 })
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 const start = async () => {
 
   await app.listen(PORT)
