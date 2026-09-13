20f2106dc6f94b7dbfad21dfaf253bf61716cfba
Mohammed Alqadi <120240112@std.up.edu.ps>
2026-09-13 16:15:00 +0300

    update version

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
-
-// get the port from env variable
 const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 const start = async () => {
+
   await app.listen(PORT)
 
   console.log(`server started on port ${PORT}`)
