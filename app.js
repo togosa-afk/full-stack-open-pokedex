c545016b28f82445cde62751b9095120b9c1cfd3
Mohammed Alqadi <120240112@std.up.edu.ps>
2026-09-14 14:24:45 +0300

    Change version to 3 for PR check

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('2')
+  res.send('3')
 })
 
 app.get('/health', (req, res) => {
